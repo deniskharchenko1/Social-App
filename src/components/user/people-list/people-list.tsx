aee94b6554aed1bdab7adbd10768b5e5186fd469
denis <denis.kharchenko1@gmail.com>
2021-09-17 15:43:06 +0700

    add search panel

--- src/components/user/people-list/people-list.tsx
+++ src/components/user/people-list/people-list.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import Spinner from "../../spinner";
@@ -23,13 +23,31 @@ const PeopleList: FC = () => {
     dispatch(getUsers());
   }, [dispatch]);
 
+  const [value, setValue] = useState("");
+
+  const searchedUsers = users.filter((user) => {
+    return user.name.toLocaleLowerCase().includes(value.toLocaleLowerCase());
+  });
+
   return isLoading ? (
     <Spinner />
   ) : (
     <>
+      <div className="d-flex col-md-12">
+        <form>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              placeholder="Search user"
+              className="form-control"
+              onChange={(event) => setValue(event.target.value)}
+            />
+          </div>
+        </form>
+      </div>
       <div className="col-md-6">
         <ul className="item-list list-group">
-          {users.map((user) => (
+          {searchedUsers.map((user) => (
             <PeopleItem key={user.id} peopleData={user} />
           ))}
         </ul>
